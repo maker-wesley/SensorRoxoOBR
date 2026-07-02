7a12a524669e465148c02cf722b53fe1c210fb6a
maker-wesley <maker.wesleyoliveira@educbarueri.sp.gov.br>
2026-07-02 11:25:13 -0300

    Update leiturasimples.ino

--- examples/leituraSimples/leiturasimples.ino
+++ examples/leituraSimples/leiturasimples.ino
@@ -1,1 +1,23 @@
+#include <Wire.h>
+#include <SensorRoxoOBR.h>
 
+SensorRoxoOBR sensorEsquerdo(0x70, 0, 50, 4);
+SensorROxoOBR sensorDireito(0x70, 3, 50, 4);
+
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin();
+
+    if(!sensorEsquerdo.inicializar()) Serial.println("Sensor esquerdo NAO encontrado!");
+    if(!sensorDireito.inicializar()) Serial.println("Sensor direito NAO encontrado!");
+}
+
+void loop()
+{
+    String coleta_esquerdo = sensorEsquerdo.coletar();
+    String coleta_direito = sensorDireito.coletar();
+
+    Serial.print("Valores para calibrar sensor esquerdo: ") Serial.print(coleta_esquerdo);
+    Serial.print(" || Valores para calibrar sensor direito: ") Serial.println(coleta_direito);
+}
