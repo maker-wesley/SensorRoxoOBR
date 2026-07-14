3a2de806d135aa30eee3ace63f66aa26f1823aac
maker-wesley <maker.wesleyoliveira@educbarueri.sp.gov.br>
2026-07-14 11:30:11 -0300

    Update leituraSimples.ino

--- examples/leituraSimples/leituraSimples.ino
+++ examples/leituraSimples/leituraSimples.ino
@@ -1,22 +1,22 @@
 #include <SensorRoxoOBR.h>
 
-SensorRoxoOBR sensorEsquerdo(0x70, 0, 50, 4);
-SensorROxoOBR sensorDireito(0x70, 3, 50, 4);
+SensorRoxoOBR sensor0(0x70, 0, 50, 4);
+SensorRoxoOBR sensor3(0x70, 3, 50, 4);
 
 void setup()
 {
     Serial.begin(9600);
     Wire.begin();
 
-    if(!sensorEsquerdo.inicializar()) Serial.println("Sensor esquerdo NAO encontrado!");
-    if(!sensorDireito.inicializar()) Serial.println("Sensor direito NAO encontrado!");
+    if(!sensor0.inicializar()) Serial.println("Sensor 0 NAO encontrado!");
+    if(!sensor3.inicializar()) Serial.println("Sensor 3 NAO encontrado!");
 }
 
 void loop()
 {
-    String coleta_esquerdo = sensorEsquerdo.coletar();
-    String coleta_direito = sensorDireito.coletar();
+    String coleta0 = sensor0.coletar();
+    String coleta3 = sensor3.coletar();
 
-    Serial.print("Valores para calibrar sensor esquerdo: ") Serial.print(coleta_esquerdo);
-    Serial.print(" || Valores para calibrar sensor direito: ") Serial.println(coleta_direito);
+    Serial.print("Valores para calibrar sensor esquerdo: ") Serial.print(coleta0);
+    Serial.print(" || Valores para calibrar sensor direito: ") Serial.println(coleta3);
 }
