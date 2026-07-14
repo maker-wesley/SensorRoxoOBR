8a7d8c4c165300884a5a12f25c1c7c305168cb17
maker-wesley <maker.wesleyoliveira@educbarueri.sp.gov.br>
2026-07-14 11:30:56 -0300

    Update identificandoCor.ino

--- examples/identificandoCor/identificandoCor.ino
+++ examples/identificandoCor/identificandoCor.ino
@@ -1,25 +1,25 @@
 #include <SensorRoxoOBR.h>
 
-SensorRoxoOBR sensorEsquerdo(0x70, 0, 50, 4);
-SensorRoxoOBR sensorDireito(0x70, 3, 50, 4);
+SensorRoxoOBR sensor0(0x70, 0, 50, 4);
+SensorRoxoOBR sensor3(0x70, 3, 50, 4);
 
 void setup()
 {
     Serial.begin(9600);
     Wire.begin();
 
-    if(!sensorEsquerdo.inicializar()) Serial.println("Sensor esquerdo NAO encontrado!");
-    if(!sensorDireito.inicializar()) Serial.println("Sensor direito NAO encontrado!");
+    if(!sensor0.inicializar()) Serial.println("Sensor 0 NAO encontrado!");
+    if(!sensor3.inicializar()) Serial.println("Sensor 3 NAO encontrado!");
 
-    sensorEsquerdo.calibrar(4000, 800, 0.50, 0.43, 0.40);
-    sensorDireito.calibrar(4000, 800, 0.50, 0.43, 0.40);
+    sensor0.calibrar(4000, 800, 0.50, 0.43, 0.40);
+    sensor3.calibrar(4000, 800, 0.50, 0.43, 0.40);
 }
 
 void loop()
 {
-    String cor_esquerda = sensorEsquerdo.cor();
-    String cor_direita = sensorDireito.cor();
+    String cor0 = sensor0.cor();
+    String cor3 = sensor3.cor();
 
-    Serial.print("Sensor esquerdo: "); Serial.print(cor_esquerda);
-    Serial.print(" || Sensor direito: "); Serial.println(cor_direita);
+    Serial.print("Sensor esquerdo: "); Serial.print(cor0);
+    Serial.print(" || Sensor direito: "); Serial.println(cor3);
 }
